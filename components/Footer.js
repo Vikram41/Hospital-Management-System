997f7119c1d9e9b93b6d81e14820cdd0f9af90c5
Vikram41 <90099207+Vikram41@users.noreply.github.com>
2025-02-04 14:03:47 +0530

    Delete components directory

--- components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="footer">
-        <p>&copy; {new Date().getFullYear()} Hospital Management System</p>
-      </footer>
-    );
-  }
-  
